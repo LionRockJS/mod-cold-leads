522a608f9b6f0b75e6bee440e9270ef8554e26f6
Colin Leung <colin@cowise.co>
2025-08-24 04:37:56 +0800

    update schema

--- schema/index.js
+++ schema/index.js
@@ -1,29 +1,6 @@
-import url from "node:url";
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
-
 import path from 'node:path';
-import { build } from '@lionrockjs/start';
+import {build} from '@lionrockjs/start';
 
-build(
-  `${__dirname}/lead.graphql`,
-  `${__dirname}/lead.mjs`,
-  `${__dirname}/exports/lead.sql`,
-  `${__dirname}/default/db/www/lead.sqlite`,
-  path.normalize(`${__dirname}/model`)
-);
+const __dirname = path.dirname(import.meta.url).replace("file://", "");
 
-build(
-  `${__dirname}/lead_info.graphql`,
-  ``,
-  `${__dirname}/exports/lead_info.sql`,
-  `${__dirname}/default/db/www/lead_info.sqlite`,
-  path.normalize(`${__dirname}/model`)
-);
-
-build(
-  `${__dirname}/lead_action.graphql`,
-  ``,
-  `${__dirname}/exports/lead_action.sql`,
-  `${__dirname}/default/db/www/lead_action.sqlite`,
-  path.normalize(`${__dirname}/model`)
-);
+build(__dirname, '/..', 'lead', 'lead/lead', 'www', true);
