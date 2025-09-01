1fd55a3b92066f362eb59da3e22835b97c24244e
Colin Leung <colin@cowise.co>
2025-09-01 22:05:20 +0800

    Remove unused LeadAction and LeadInfo exports
    Eliminated ModelLeadAction and ModelLeadInfo imports and exports from index.js as they are no longer needed. Bumped package version to 0.0.14.

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import ControllerLead from './classes/controller/Lead.mjs';
 import HelperEdm from './classes/helper/Edm.mjs';
 import HelperLead from './classes/helper/Lead.mjs';
 import ModelLead from './classes/model/Lead.mjs';
-import ModelLeadAction from './classes/model/LeadAction.mjs';
-import ModelLeadInfo from './classes/model/LeadInfo.mjs';
 import ModelLeadState from './classes/model/LeadState.mjs';
 import ModelLeadType from './classes/model/LeadType.mjs';
 
@@ -17,8 +15,6 @@ export {
   HelperEdm,
   HelperLead,
   ModelLead,
-  ModelLeadAction,
-  ModelLeadInfo,
   ModelLeadState,
   ModelLeadType
 };
